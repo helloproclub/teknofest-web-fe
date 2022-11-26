4875f8c75eda40e28a67ea02ae35b81e0d10a5be
LvlyCroissant <43910519+afasyah@users.noreply.github.com>
2022-11-26 22:49:45 +0700

    update extend regist

--- src/helpers/registration-close-helper.js
+++ src/helpers/registration-close-helper.js
@@ -1,14 +1,23 @@
 import moment from 'moment';
 
 export const isRegistClosed = () => {
+    let status:
     const now = moment();
     const closed = moment('2022-11-27 00:01');
+    const reopen = moment('2022-11-27 09:00');
+    const closed2 = moment('2022-11-29 00:01');
 
     if (now > closed) {
-        return true;
+        if (now >= reopen && now < closed2) {
+            status = false;
+        } else {
+            status = true;
+        }
+    } else {
+        status = false;
     }
 
-    return false;
+    return status;
 }
 
 export const registEvent = () => {
